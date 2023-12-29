8ca4752c4168da158b8dc233453ad6671b26b4f2
Milena Mello <milenamello625@gmail.com>
2023-12-29 15:49:08 +0300

    Display phrase on game board

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -2,4 +2,23 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+
+/**
+* Display phrase on game board
+*/
+    addPhraseToDisplay() {
+        const phraseUl = document.querySelector('#phrase ul');
+        const phraseLetters = this.phrase.split('');
+        phraseLetters.forEach(letter => {
+            const li = document.createElement('li');
+            li.textContent = letter;
+            if (letter === ' ') {
+                li.className = 'space';
+            } else {
+                li.className = `hide letter ${letter}`;
+            }
+            phraseUl.appendChild(li);
+        });
+    }
 }
+
